e149d45356823ec0dde2864a80fa153cc6c901d5
Cary Phillips <cary@ilm.com>
2020-08-05 18:18:25 -0700

    Disable clang-format for python bindings
    Since the long-term plan for Iath python bindings is to migrate to
    pybind11, we don't expect this code to change much, or even really to
    be examined much at all, so better to leave it as is, reducing the
    risk of inadvertently introducing bugs or obscuring the revision
    history.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- python/PyImath/PyImathStringArrayRegister.h
+++ python/PyImath/PyImathStringArrayRegister.h
@@ -2,9 +2,9 @@
 //
 // Copyright (c) 2009-2012, Industrial Light & Magic, a division of Lucas
 // Digital Ltd. LLC
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -16,8 +16,8 @@
 // distribution.
 // *       Neither the name of Industrial Light & Magic nor the names of
 // its contributors may be used to endorse or promote products derived
-// from this software without specific prior written permission. 
-// 
+// from this software without specific prior written permission.
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -31,6 +31,8 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 ///////////////////////////////////////////////////////////////////////////
+
+// clang-format off
 
 #ifndef _PyImathStringArrayRegister_h_
 #define _PyImathStringArrayRegister_h_
