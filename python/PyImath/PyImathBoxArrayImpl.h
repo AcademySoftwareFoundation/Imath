7d89d087796ef1323655f901ba420809f11195ca
Cary Phillips <cary@ilm.com>
2020-08-05 18:18:25 -0700

    Disable clang-format for python bindings
    Since the long-term plan for Iath python bindings is to migrate to
    pybind11, we don't expect this code to change much, or even really to
    be examined much at all, so better to leave it as is, reducing the
    risk of inadvertently introducing bugs or obscuring the revision
    history.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- python/PyImath/PyImathBoxArrayImpl.h
+++ python/PyImath/PyImathBoxArrayImpl.h
@@ -1,3 +1,4 @@
+
 #ifndef _PyImathBoxArrayImpl_h_
 #define _PyImathBoxArrayImpl_h_
 
@@ -5,9 +6,9 @@
 //
 // Copyright (c) 1998-2011, Industrial Light & Magic, a division of Lucas
 // Digital Ltd. LLC
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -19,8 +20,8 @@
 // distribution.
 // *       Neither the name of Industrial Light & Magic nor the names of
 // its contributors may be used to endorse or promote products derived
-// from this software without specific prior written permission. 
-// 
+// from this software without specific prior written permission.
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -40,6 +41,8 @@
 // of the various Box* types can be spread across multiple files in
 // order to work around MSVC limitations.
 //
+
+// clang-format off
 
 #include "PyImathBox.h"
 #include "PyImathDecorators.h"
