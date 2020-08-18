89b6208fa7929651d0cd223dee201d6481ff72b6
Cary Phillips <cary@ilm.com>
2020-08-18 11:55:21 -0700

    Fix typos in Doxyfile and conf.py
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ extensions = [
 ]
 
 # Breathe extension variables
-breathe_projects = { "Imath-test": "doxyxml/" }
-breathe_default_project = "Imath-test"
+breathe_projects = { "Imath": "doxyxml/" }
+breathe_default_project = "Imath"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -60,7 +60,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Imath-test'
+project = 'Imath'
 copyright = '2020, Cary Phillips'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -193,7 +193,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Imath-test'
+htmlhelp_basename = 'Imath'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -213,8 +213,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Imath-test.tex', 'Imath-test Documentation',
-   'Thomas Edvalson', 'manual'),
+  ('index', 'Imath.tex', 'Imath Documentation',
+   'Cary Phillips', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -243,8 +243,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'imath-test', 'Imath-test Documentation',
-     ['Thomas Edvalson'], 1)
+    ('index', 'imath', 'Imath Documentation',
+     ['Cary Phillips'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -257,8 +257,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Imath-test', 'Imath-test Documentation',
-   'Thomas Edvalson', 'Imath-test', 'One line description of project.',
+  ('index', 'Imath', 'Imath Documentation',
+   'Cary Phillips', 'Imath', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,10 +1,8 @@
-.. Imath-teset documentation master file, created by
+.. Imath documentation master file, created by
    sphinx-quickstart on Wed Apr 24 15:19:01 2019.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
-Imath-test API Documentation
-============================
+Imath Documentation
+===================
 
 .. toctree::
    :maxdepth: 3
