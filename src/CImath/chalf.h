575349642d49f9f9616ee0c429d124b3b45a984a
Scott Wilson <scott@propersquid.com>
2020-10-25 14:17:01 -0700

    feat: Implement half constructors in C.
    Signed-off-by: Scott Wilson <scott@propersquid.com>

---
+++ src/CImath/chalf.h
@@ -0,0 +1,20 @@
+#ifndef _CHALF_H_
+#define _CHALF_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    typedef struct
+    {
+        unsigned short _h;
+    } imath_half;
+
+    imath_half imath_half_createFromFloat (float f);
+    imath_half imath_half_copy (const imath_half* h);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
