72121b1017ef6c28d50bf0498c70936494aef93f
Cary Phillips <cary@ilm.com>
2021-01-12 10:11:04 -0800

    Incorrect constexpr in Imath::limits<half>, and missing test. (#76)
    * Incorrect constexpr in ImathHalfLimits.h
    
    Presumably a typo, constexpr where IMATH_HOSTDEVICE should
    appear.
    
    Also, this was never caught because this file is not included by any
    other source files or by any tests.
    
    Signed-off-by: Cary Phillips <cary@ilm.com>
    
    * add testHalfLimits() to test Imath:limits<half>
    
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/Imath/ImathHalfLimits.h
+++ src/Imath/ImathHalfLimits.h
@@ -50,12 +50,12 @@ IMATH_INTERNAL_NAMESPACE_HEADER_ENTER
 
 template <> struct limits<half>
 {
-    constexpr static constexpr float min() noexcept { return -HALF_MAX; }
-    constexpr static constexpr float max() noexcept { return HALF_MAX; }
-    constexpr static constexpr float smallest() noexcept { return HALF_MIN; }
-    constexpr static constexpr float epsilon() noexcept { return HALF_EPSILON; }
-    constexpr static constexpr bool isIntegral() noexcept { return false; }
-    constexpr static constexpr bool isSigned() noexcept { return true; }
+    IMATH_HOSTDEVICE static constexpr float min() noexcept { return -HALF_MAX; }
+    IMATH_HOSTDEVICE static constexpr float max() noexcept { return HALF_MAX; }
+    IMATH_HOSTDEVICE static constexpr float smallest() noexcept { return HALF_MIN; }
+    IMATH_HOSTDEVICE static constexpr float epsilon() noexcept { return HALF_EPSILON; }
+    IMATH_HOSTDEVICE static constexpr bool isIntegral() noexcept { return false; }
+    IMATH_HOSTDEVICE static constexpr bool isSigned() noexcept { return true; }
 };
 
 IMATH_INTERNAL_NAMESPACE_HEADER_EXIT
