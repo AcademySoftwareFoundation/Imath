ea1f19fd0c04dfbf3f272860ff2ffe5ba7d2fd9f
Bernd Waibel <waebbl-gentoo@posteo.net>
2021-02-23 00:48:20 +0100

    clean-up redundant search for clang-tidy
    Signed-off-by: Bernd Waibel <waebbl-gentoo@posteo.net>

--- src/python/config/PyImathSetup.cmake
+++ src/python/config/PyImathSetup.cmake
@@ -15,25 +15,8 @@ set(PYIMATH_OVERRIDE_PYTHON3_INSTALL_DIR "" CACHE STRING "Override the install l
 
 # Suffix to append to root name, this helps with version management
 # but can be turned off if you don't care, or otherwise customized
-# 
+#
 set(PYIMATH_LIB_SUFFIX "-${IMATH_VERSION_API}" CACHE STRING "String added to the end of all the libraries")
 # This provides a root for the unique name of the library based on
 # the version of python being compiled for
 set(PYIMATH_LIB_PYTHONVER_ROOT "_Python" CACHE STRING "String added as a root to the identifier of the python version in the libraries")
-
-########################
-
-# Code check related features
-option(OPENEXR_USE_CLANG_TIDY "Check if clang-tidy is available, and enable that" OFF)
-if(OPENEXR_USE_CLANG_TIDY)
-  find_program(OPENEXR_CLANG_TIDY_BIN clang-tidy)
-  if(OPENEXR_CLANG_TIDY_BIN-NOTFOUND)
-    message(FATAL_ERROR "clang-tidy processing requested, but no clang-tidy found")
-  endif()
-  # TODO: Need to define the list of valid checks and add a file with said list
-  set(CMAKE_CXX_CLANG_TIDY
-    ${OPENEXR_CLANG_TIDY_BIN};
-    -header-filter=.;
-    -checks=*;
-  )
-endif()
