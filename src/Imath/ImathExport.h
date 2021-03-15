41c027cff111b26d0d51e8b9faae71bcdc13726e
Kimball Thurston <kdt3rd@gmail.com>
2021-03-15 21:12:50 +1300

    Move option to config.h, simplifying export macro declaration code
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- src/Imath/ImathExport.h
+++ src/Imath/ImathExport.h
@@ -6,6 +6,8 @@
 #ifndef INCLUDED_IMATHEXPORT_H
 #define INCLUDED_IMATHEXPORT_H
 
+#include "ImathConfig.h"
+
 /// \defgroup ExportMacros Macros to manage symbol visibility
 ///
 /// There is more information about the motivation for these macros
@@ -34,51 +36,32 @@
 #    define IMATH_EXPORT_CONST extern __declspec(dllimport)
 #  endif
 
+// DLLs don't support these types of visibility controls, just leave them as empty
+#  define IMATH_EXPORT_TYPE
+#  define IMATH_EXPORT_ENUM
+#  define IMATH_EXPORT_TEMPLATE_TYPE
+
 #else
 
-// Need to handle when we are building as a static library under
-// windows and with a compiler that doesn't understand visibility
-// attributes.
-#  ifndef _MSC_VER
-// Allow the user to override and not control visibility
-#    ifdef IMATH_USE_DEFAULT_VISIBILITY
-#      define IMATH_EXPORT
-#      define IMATH_EXPORT_CONST extern const
-// else We are using a compiler that knows about visibility, so works for both
-// shared and static libraries...
-#    else // IMATH_USE_DEFAULT_VISIBILITY
-#      define IMATH_EXPORT __attribute__ ((__visibility__ ("default")))
-#      define IMATH_EXPORT_TYPE __attribute__ ((__visibility__ ("default")))
-#      define IMATH_HIDDEN __attribute__ ((__visibility__ ("hidden")))
-// clang differs from gcc and has type visibility which is needed for enums and templates
-#      if __has_attribute(__type_visibility__)
-#        define IMATH_EXPORT_ENUM __attribute__ ((__type_visibility__ ("default")))
-#        define IMATH_EXPORT_TEMPLATE_TYPE __attribute__ ((__type_visibility__ ("default")))
-#      else
-#        define IMATH_EXPORT_TEMPLATE_TYPE __attribute__ ((__visibility__ ("default")))
-#      endif
-#      define IMATH_EXPORT_CONST extern const __attribute__ ((visibility ("default")))
-#    endif
-#  else // _MSC_VER
+#  ifdef IMATH_PUBLIC_SYMBOL_ATTRIBUTE
+#    define IMATH_EXPORT IMATH_PUBLIC_SYMBOL_ATTRIBUTE
+#    define IMATH_EXPORT_CONST extern const  IMATH_PUBLIC_SYMBOL_ATTRIBUTE
+#  else
 #    define IMATH_EXPORT
 #    define IMATH_EXPORT_CONST extern const
 #  endif
 
-#endif // IMATH_DLL
-
-// Provide defaults so we don't have to replicate lines as much
-#ifndef IMATH_EXPORT_TYPE
-#    define IMATH_EXPORT_TYPE
-#endif
-#ifndef IMATH_EXPORT_TEMPLATE_TYPE
-#    define IMATH_EXPORT_TEMPLATE_TYPE
-#endif
-#ifndef IMATH_EXPORT_ENUM
+#  ifdef IMATH_PUBLIC_TYPE_VISIBILITY_ATTRIBUTE
+#    define IMATH_EXPORT_ENUM IMATH_PUBLIC_TYPE_VISIBILITY_ATTRIBUTE
+#    define IMATH_EXPORT_TEMPLATE_TYPE IMATH_PUBLIC_TYPE_VISIBILITY_ATTRIBUTE
+#    define IMATH_EXPORT_TYPE IMATH_PUBLIC_TYPE_VISIBILITY_ATTRIBUTE
+#  else
 #    define IMATH_EXPORT_ENUM
-#endif
-#ifndef IMATH_HIDDEN
-#    define IMATH_HIDDEN
-#endif
+#    define IMATH_EXPORT_TEMPLATE_TYPE IMATH_EXPORT
+#    define IMATH_EXPORT_TYPE IMATH_EXPORT
+#  endif
+
+#endif // IMATH_DLL
 
 /// @}
 
