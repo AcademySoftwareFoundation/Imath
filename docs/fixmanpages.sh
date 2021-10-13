9a5a77437cc18b5ca1c066950f999a3290a46593
Cary Phillips <cary@ilm.com>
2021-10-12 18:34:21 -0700

    Accidentally left GENERATE_MAN=ON, and fixmanpage.sh handles missing directory
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- docs/fixmanpages.sh
+++ docs/fixmanpages.sh
@@ -4,12 +4,18 @@
 # * Strip "_ Vec" and "_ T _ _" from "Imath_Box_ Vec2_ T _ _.3"
 # * and rename "Imath_*" to "Imath::*"
 
-cd $1/man/man3
-for file in Imath_*.3;
-do
-    new=`echo $file | sed -e 's/_ T _ _//g' -e 's/_ Vec//g' -e s/_/::/g`
-    /bin/mv "$file" $new
-done
+if [ -d $1/man/man3 ]; then
 
+  cd $1/man/man3
+  echo cd $1/man/man3
+  shopt -s nullglob
+  for file in Imath_*.3;
+  do
+    new=`echo $file | sed -e 's/_ T _ _//g' -e 's/_ Vec//g' -e s/_/::/g`
+    echo /bin/mv "$file" $new
+    /bin/mv "$file" $new
+  done
+fi
+   
 
 
