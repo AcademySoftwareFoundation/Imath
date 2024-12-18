f6edc6a5dc895b2e9358cf546f6b9a94fd176bcd
Todica Ionut <todicaionut2000111@gmail.com>
2024-12-18 19:20:46 +0200

    remove Euler
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>

--- src/pybind11/PyBindImath/pybindimathmodule.cpp
+++ src/pybind11/PyBindImath/pybindimathmodule.cpp
@@ -15,7 +15,7 @@ PYBIND11_MODULE(pybindimath, m)
     PyBindImath::register_imath_box(m);
     PyBindImath::register_imath_plane(m);
     PyBindImath::register_imath_line(m);
-    PyBindImath::register_imath_euler(m);
+    // PyBindImath::register_imath_euler(m)
     // PyBindImath::register_imath_frustum(m)
 
 
