dac2eab9af8a85f1bf4b09d5555c13d66936da65
Todica Ionut <todicaionut2000111@gmail.com>
2024-12-19 06:53:42 +0200

    Fix Pybind11 Imath Frustum (#459)
    * Imath Frustum to IMATH_NAMESPACE
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * Imath Euler to IMATH_NAMESPACE
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * Remove
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * Restore
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * Frustum class S
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * Frustum remove class M
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * bug fix
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * bug fix
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * bug fix
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * bug fix
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * bug fix
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * bug fix
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * add float double
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * remove Frustum
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * remove Frustum
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * remove PyBind Imath Frustum
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * remove Euler
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * remove Euler
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    * remove Euler pybind test
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    
    ---------
    
    Signed-off-by: Todica Ionut <todicaionut2000111@gmail.com>
    Signed-off-by: Treata11 <treata11@yahoo.com>

--- src/pybind11/PyBindImath/pybindimathmodule.cpp
+++ src/pybind11/PyBindImath/pybindimathmodule.cpp
@@ -15,8 +15,8 @@ PYBIND11_MODULE(pybindimath, m)
     PyBindImath::register_imath_box(m);
     PyBindImath::register_imath_plane(m);
     PyBindImath::register_imath_line(m);
-    PyBindImath::register_imath_euler(m);
-    PyBindImath::register_imath_frustum(m);
+    // PyBindImath::register_imath_euler(m)
+    // PyBindImath::register_imath_frustum(m)
 
 
     //
